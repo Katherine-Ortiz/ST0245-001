09597214e5385d8686c9a70352e086b9945afd9d
vochoaa <78158822+vochoaa@users.noreply.github.com>
2021-02-25 18:56:28 -0500

    Add files via upload

---
+++ laboratorios/lab01/ejercicioEnLinea/Ejercicio4.java
@@ -0,0 +1,10 @@
+public class Ejercicio4
+{  public boolean groupNoAdj(int start, int[] nums, int target) {
+        if(start >= nums.length)
+            return target == 0;
+        else
+            return (groupNoAdj(start + 2, nums, target - nums[start]) ||
+                    groupNoAdj(start + 1, nums, target));
+    }
+   
+}
