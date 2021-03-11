c01ea7fce686b10c14bfd8c6d30d6bb706344d25
Katherine-Ortiz <kbenjumeao@eafit.edu.co>
2021-03-10 19:31:01 -0500

    Array2

---
+++ laboratorios/lab02/Sum28.java
@@ -0,0 +1,16 @@
+public class array2 {
+
+    public boolean sum28(int[] nums){
+    int sum = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] == 2){
+                sum += nums[i];
+            }
+        }
+            if(sum == 8){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
