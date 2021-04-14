309e80068d2e731dfd4595acc9e09a18d1dc0d11
vochoaa <78158822+vochoaa@users.noreply.github.com>
2021-04-14 16:21:32 -0500

    Delete proyecto.py

--- proyecto/codigo/proyecto.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-import pandas as pd
-ruta = './Ganado.CSV'
-arreglo= os.listdir(ruta)
-archivos=[[]]*len(arreglo)
-for i in range(len(arreglo)):
-   archivos[i]=pd.read_csv(ruta +'/'+ arreglo[i])
-print(archivos)
-
